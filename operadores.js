bc2799d83c82ca74e6e99f520ca444c10044d72d
FMendezAco <mendezflxvxx14@gmail.com>
2023-02-08 23:16:02 -0500

    chechpoint commit

---
+++ operadores.js
@@ -0,0 +1,10 @@
+console.log( 3 + 5 );
+console.log( 4 / 2 )
+console.log(    5 - 3 );
+console.log( 3* 9 )
+console.log ( 96 % 4 )
+console.log (( 3 + 5) * 2 - 8 )
+console.log("Hola" + "Camilo");
+    var nombre =  "Camilo";    
+    var saludo =  "Hola" + nombre;
+    console.log(saludo);
